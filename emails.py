3dbdbbac287325e146cd5de4336fb6a9854f602d
RZzero <rafaelsuazoh@gmail.com>
2017-04-13 20:34:25 -0400

    ADDED: Support for attached files

--- emails.py
+++ emails.py
@@ -5,6 +5,8 @@
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
+from email.mime.application import MIMEApplication
+from os.path import basename
 from message import Message
 
                     #  message refers to a message object
@@ -16,6 +18,16 @@ def send_email_gmail(user_mail, user_pass, message):
     msg['Subject'] = message.subject
     msg_body = message.content
     msg.attach(MIMEText(msg_body, 'plain'))
+
+    files = [""]
+    if len(files) > 0:
+        for f in files:
+            with open(f, "r") as fil:
+                part = MIMEApplication(
+                    fil.read(),
+                )
+                part['Content-Disposition'] = 'attachment; filename="{0}"'.format(basename(f))
+                msg.attach(part)
 
     try:
         server = smtplib.SMTP_SSL('smtp.gmail.com:465')
